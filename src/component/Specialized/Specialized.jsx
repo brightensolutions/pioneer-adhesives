6979cc2c34159e48e639ab14c1da531bd4edf518
manthan <designer.brightensolutions@gmail.com>
2024-09-11 13:09:49 +0530

    main slider auto slide

--- src/component/Specialized/Specialized.jsx
+++ src/component/Specialized/Specialized.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
 import { motion } from "framer-motion";
-import { akzonobel, belzonaimg, bg_img_slider, bg_pluse, logo2, Mcompany, slider1, slider2, slider3, slider4 } from "../../Images/Images";
+import { akzonobel, belzonaimg, Mcompany, slider1, slider2, slider3 } from "../../Images/Images";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -12,7 +12,7 @@ const Specialized = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 1500,
     fade: true,
   };
@@ -21,8 +21,8 @@ const Specialized = () => {
     {
       bgImage: slider1,
       content: (
-        <div className="text-center sm:max-w-[500px] max-w-[350px] p-[10px]">
-          <h1 className="text-white sm:text-[58px] text-[30px] font-bold">Pioneer Adhesives</h1>
+        <div className="text-center sm:max-w-[500px] max-w-[350px] p-[10px] sm:p-0">
+          <h1 className="text-white sm:text-[52px] text-[30px] font-bold">Pioneer Adhesives</h1>
           <h4 className="text-white font-bold sm:text-[30px] text-[25px]">D & H Secheron</h4>
           <p className="text-white sm:text-[20px] text-[18px]">Pioneer Adhesives is a trusted distributor of DNH Secheron, offering top-quality welding and industrial solutions. We provide innovative products designed to meet the needs of various industries.</p>
         </div>
@@ -32,7 +32,7 @@ const Specialized = () => {
       bgImage: belzonaimg,
       content: (
         <div className="text-center sm:max-w-[500px] max-w-[350px] p-[10px]">
-          <h1 className="text-white sm:text-[58px] text-[30px] font-bold">Pioneer Adhesives</h1>
+          <h1 className="text-white sm:text-[52px] text-[30px] font-bold">Pioneer Adhesives</h1>
           <h4 className="text-white font-bold sm:text-[30px] text-[25px]">Belzona</h4>
           <p className="text-white sm:text-[20px] text-[18px]">Pioneer Adhesives proudly distributes Belzona products, offering advanced repair and protection solutions for machinery, equipment, and structures. Our range ensures long-lasting performance in various industrial applications.</p>
         </div>
@@ -42,7 +42,7 @@ const Specialized = () => {
       bgImage: Mcompany,
       content: (
         <div className="text-center sm:max-w-[500px] max-w-[350px] p-[10px]">
-          <h1 className="text-white sm:text-[58px] text-[30px] font-bold">Pioneer Adhesives</h1>
+          <h1 className="text-white sm:text-[52px] text-[30px] font-bold">Pioneer Adhesives</h1>
           <h4 className="text-white font-bold sm:text-[30px] text-[25px]">3M Company</h4>
           <p className="text-white sm:text-[20px] text-[18px]">Pioneer Adhesives supplies high-quality safety gear from 3M and advanced repair solutions from Belzona, ensuring reliable protection for both personal safety and machinery.</p>
         </div>
@@ -52,18 +52,17 @@ const Specialized = () => {
       bgImage: akzonobel,
       content: (
         <div className="text-center sm:max-w-[500px] max-w-[350px] p-[10px]">
-          <h1 className="text-white sm:text-[58px] text-[30px] font-bold">Akzonobel</h1>
+          <h1 className="text-white sm:text-[52px] text-[30px] font-bold">Akzonobel</h1>
           <h4 className="text-white font-bold text-[30px]">3M Company</h4>
           <p className="text-white sm:text-[20px] text-[18px]">Pioneer Adhesives provides AkzoNobel's Nordsjö paints for Artscape’s urban art projects. These vibrant murals, created with our high-quality paints, enhance cityscapes and foster community spirit.</p>
         </div>
       ),
     },
     {
-      bgImage: slider2
+      bgImage: slider2,
     },
     {
       bgImage: slider3,
-
     },
   ];
 
@@ -72,7 +71,7 @@ const Specialized = () => {
       {/* Slider Wrapper */}
       <Slider {...sliderSettings}>
         {slides.map((slide, index) => (
-          <div key={index} className="relative w-full md:h-[600px] h-[400px]">
+          <div key={index} className="relative w-full h-[400px] md:h-[600px]">
             {/* Background Image */}
             <motion.div
               className="absolute inset-0 bg-cover bg-center"
@@ -85,9 +84,11 @@ const Specialized = () => {
             />
 
             {/* Content */}
-            <div className="absolute top-[18%] flex flex-col items-end justify-end z-10 right-0 bg-[#0000005c] md:p-[65px] border-[2px] border-r-0">
-              {slide.content}
-            </div>
+            {slide.content && (
+              <div className="absolute top-[18%] flex flex-col items-end justify-end z-10 right-0 bg-[#0000005c] md:p-[65px] p-[10px] border-[2px] border-r-0">
+                {slide.content}
+              </div>
+            )}
           </div>
         ))}
       </Slider>
