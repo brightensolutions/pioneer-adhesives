27dd27e48a9f862929b720dff7c84e04493ebf8b
Devbrighten <developer.brightensolutions@gmail.com>
2025-12-20 14:59:42 +0530

    asianpaints

--- src/component/Home_Products/Home_Products.jsx
+++ src/component/Home_Products/Home_Products.jsx
@@ -1,8 +1,45 @@
 import React from "react";
-import { antiseizetapes, auto_light, barger, CommercialConstruction, dhlogo, electrical_tapes, hempale, hylomarproducts, industrialcleaner, Interior, ips_product, LeakRepai, mas, MASSEALINGSYSTEMS, molygraph, mseal, paintproduct, partner2, partner5, partner8, partner9, product1, product2, product3, product4, product5, product6, product7, prolite, Rubber_Repair_Protection, soudal, soudalproduct, threem, threeMsefty, Welding_Consumables } from "../../Images/Images";
+import {
+  antiseizetapes,
+  auto_light,
+  barger,
+  AsianPaints,
+  CommercialConstruction,
+  dhlogo,
+  electrical_tapes,
+  hempale,
+  hylomarproducts,
+  industrialcleaner,
+  Interior,
+  ips_product,
+  LeakRepai,
+  mas,
+  MASSEALINGSYSTEMS,
+  molygraph,
+  mseal,
+  paintproduct,
+  partner2,
+  partner5,
+  partner8,
+  partner9,
+  product1,
+  product2,
+  product3,
+  product4,
+  product5,
+  product6,
+  product7,
+  prolite,
+  Rubber_Repair_Protection,
+  soudal,
+  soudalproduct,
+  threem,
+  threeMsefty,
+  Welding_Consumables,
+} from "../../Images/Images";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
-import { Link, useLocation } from "react-router-dom";  // Import useLocation
+import { Link, useLocation } from "react-router-dom"; // Import useLocation
 import { SectionTitle } from "../Function/Function";
 
 const ProductItem = ({ image, title, logo, id, link }) => {
@@ -27,23 +64,19 @@ const ProductItem = ({ image, title, logo, id, link }) => {
         boxShadow: "0 10px 20px rgba(0, 0, 0, 0.2)",
         transition: { duration: 0.3 },
       }}
-      className="xl:w-[30.32%] lg:w-[33%] rounded-[10px] flex flex-col bg-white sm:w-[47.9%] " 
+      className="xl:w-[30.32%] lg:w-[33%] rounded-[10px] flex flex-col bg-white sm:w-[47.9%] "
     >
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <div className="flex justify-center items-center h-[250px] border-b-2">  
+        <div className="flex justify-center items-center h-[250px] border-b-2">
           <img
             src={image}
             alt={title}
-            className="w-[100%] h-[100%] object-cover"  
+            className="w-[100%] h-[100%] object-cover"
           />
         </div>
-        <div className="flex justify-center items-center flex-grow h-[150px]">  
+        <div className="flex justify-center items-center flex-grow h-[150px]">
           <div className="flex justify-center items-center">
-            <img
-              src={logo}
-              alt="logo"
-              className="w-[150px]"  
-            />
+            <img src={logo} alt="logo" className="w-[150px]" />
           </div>
         </div>
       </a>
@@ -51,85 +84,83 @@ const ProductItem = ({ image, title, logo, id, link }) => {
   );
 };
 
-
-
 export const products = [
   {
     id: 1,
     image: product1,
     logo: dhlogo,
-    link: "https://www.dnhsecheron.com/"
+    link: "https://www.dnhsecheron.com/",
   },
   {
     id: 2,
     image: product2,
     logo: partner5,
-    link: "https://belzona.com/en/index.aspx"
+    link: "https://belzona.com/en/index.aspx",
   },
   {
     id: 3,
     image: product3,
     logo: threem,
-    link: "https://www.3mindia.in/3M/en_IN/company-in/"
+    link: "https://www.3mindia.in/3M/en_IN/company-in/",
   },
   {
     id: 4,
     image: auto_light,
     logo: prolite,
-    link: "https://www.prolite.in/"
+    link: "https://www.prolite.in/",
   },
   {
     id: 5,
     image: product4,
     logo: partner9,
-    link: "https://www.pidilite.com/"
+    link: "https://www.pidilite.com/",
   },
   {
     id: 6,
     image: product7,
     logo: molygraph,
-    link: "https://www.molygraph.com/"
+    link: "https://www.molygraph.com/",
   },
   {
     id: 7,
     image: product5,
     logo: partner8,
-    link: "https://www.akzonobel.com/en"
+    link: "https://www.akzonobel.com/en",
   },
   {
     id: 8,
     image: product6,
     logo: hempale,
-    link: "https://www.hempel.com/"
+    link: "https://www.hempel.com/",
   },
   {
     id: 9,
     image: hylomarproducts,
     logo: partner2,
-    link: "https://hylomar.com/"
+    link: "https://hylomar.com/",
   },
   {
     id: 10,
     image: soudalproduct,
     logo: soudal,
-    link: "https://soudal.in/"
+    link: "https://soudal.in/",
   },
   {
     id: 11,
     image: Interior,
-    logo: barger,
-    link: "https://www.bergerpaints.com/"
+    logo: AsianPaints,
+    link: "https://www.asianpaints.com/colour-catalogue.html",
   },
   {
     id: 12,
     image: antiseizetapes,
     logo: mas,
-    link: "https://www.masseal.com/"
+    link: "https://www.masseal.com/",
   },
 ];
 
 const Home_Products = () => {
-  const location = useLocation();  // Get the current location
+  const location = useLocation(); // Get the current location
 
   return (
     <div className="bg-[#cfc9c954] lg:py-[100px] py-[50px] overflow-hidden">
@@ -150,7 +181,7 @@ const Home_Products = () => {
               key={product.id}
               id={product.id}
               image={product.image}
-              title={product.title} 
+              title={product.title}
               logo={product.logo}
               link={product.link}
             />
